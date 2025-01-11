ba921856846885dc07bb7ddff73fec6bb1429a24
jsteel17 <161091774+jsteel17@users.noreply.github.com>
2025-01-11 00:17:48 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["heart", "diamond", "spade", "club"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const suitSymbols = { heart: "♥", diamond: "♦", spade: "♠", club: "♣" };
+
+  const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  const randomValue = values[Math.floor(Math.random() * values.length)];
+  const suitSymbol = suitSymbols[randomSuit];
+
+  const cardElement = document.getElementById("card");
+  cardElement.classList.add(randomSuit);
+
+  cardElement.innerHTML = `
+    <div class="corner top-left">${suitSymbol}</div>
+    <div>${randomValue}</div>
+    <div class="corner bottom-right">${suitSymbol}</div>
+  `;
 };
